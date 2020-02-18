9691bd2ac93055e1c6a04c011fe16c92fb28498a
kimhyeonkyung <khk5087@naver.com>
2020-02-18 11:35:30 +0900

    Step06 fragment2 Dto 로 전달해보기

--- step06fragment2/src/main/java/com/gura/step06fragment2/MainActivity.java
+++ step06fragment2/src/main/java/com/gura/step06fragment2/MainActivity.java
@@ -12,10 +12,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
 
 import com.gura.step06fragment2.ui.main.SectionsPagerAdapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    ListView listView;
+    List<CountryDto> countries;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +30,21 @@ public class MainActivity extends AppCompatActivity {
         //레이아웃 구성하기
         setContentView(R.layout.activity_main);
 
+        listView=findViewById(R.id.listview);
+        countries=new ArrayList<>();
+        countries.add(new CountryDto(R.drawable.austria, "오스트리아", "오스트리아"));
+        countries.add(new CountryDto(R.drawable.belgium, "벨기에", "벨기에"));
+        countries.add(new CountryDto(R.drawable.brazil, "브라질", "브라질"));
+        countries.add(new CountryDto(R.drawable.france, "프랑스", "프랑스"));
+        countries.add(new CountryDto(R.drawable.germany, "독일", "독일"));
+        countries.add(new CountryDto(R.drawable.greece, "그리스", "그리스"));
+        countries.add(new CountryDto(R.drawable.italy, "이탈리아", "이탈리아"));
+        countries.add(new CountryDto(R.drawable.japan, "일본", "일본"));
+        countries.add(new CountryDto(R.drawable.poland, "폴란드", "폴란드"));
+        countries.add(new CountryDto(R.drawable.korea, "한국", "한국"));
+
         //Pager adapter 객체 생성하기(fragment를 공급해준다.)
-        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(this, getSupportFragmentManager());
+        SectionsPagerAdapter sectionsPagerAdapter = new SectionsPagerAdapter(countries, getSupportFragmentManager());
 
         //ViewPager 객체의 참조값 얻어오기(ViewPager에 fragment를 하나씩 공급해준다.)
         ViewPager viewPager = findViewById(R.id.view_pager);
