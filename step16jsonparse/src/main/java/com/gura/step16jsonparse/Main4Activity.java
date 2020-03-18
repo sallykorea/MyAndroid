efb877b236f1d4a62c1f7e5d401c5f0a3d1fa9d9
kimhyeonkyung <khk5087@naver.com>
2020-03-18 10:20:16 +0900

    step16 jsonparse 에 회원정보 입력, 삭제 기능 추가

--- step16jsonparse/src/main/java/com/gura/step16jsonparse/Main4Activity.java
+++ step16jsonparse/src/main/java/com/gura/step16jsonparse/Main4Activity.java
@@ -1,5 +1,7 @@
 package com.gura.step16jsonparse;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +9,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
@@ -17,74 +20,161 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class Main4Activity extends AppCompatActivity implements View.OnClickListener, AdapterView.OnItemClickListener, Util.RequestListener {
+public class Main4Activity extends AppCompatActivity
+        implements Util.RequestListener,
+        AdapterView.OnItemClickListener,
+        AdapterView.OnItemLongClickListener,
+        View.OnClickListener {
 
-    private ListView listView;
     private MemberAdapter adapter;
     private List<MemberDto> list;
+    //static final 상수 정의하기
+    public static final int REQUEST_LIST=0;
+    public static final int REQUEST_DELETE=1;
+    public static final int REQUEST_INSERT=2;
+    //EditText
+    EditText inputName, inputAddr;
+    //ListView
+    private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main3);
-
-        listView=findViewById(R.id.listView);
-
+        setContentView(R.layout.activity_main4);
+        //모델 객체 생성
         list=new ArrayList<>();
-
+        //아답타 객체 생성
         adapter=new MemberAdapter(this, R.layout.listview_cell, list);
+        //ListView
+        listView=findViewById(R.id.listView);
         listView.setAdapter(adapter);
-        listView.setOnItemClickListener(this);
-
-        Button getListBtn=findViewById(R.id.getList);
-        getListBtn.setOnClickListener(this);
-
-    }
-
-
-    @Override
-    public void onClick(View v) {
+        //스프링 웹서버에 요청하기
         String urlAddr="http://192.168.0.35:8865/spring05/android/member/list.do";
-        Util.sendGetRequest(0, urlAddr, null, this); //요청하면 json 형식으로 data를 응답한다.
+        Util.sendGetRequest(REQUEST_LIST, urlAddr, null, this);
+        //ListView  에 아이템 클릭 리스너 등록
+        listView.setOnItemClickListener(this);
+        listView.setOnItemLongClickListener(this);
+        //EditText
+        inputName=findViewById(R.id.inputName);
+        inputAddr=findViewById(R.id.inputAddr);
+        Button addBtn=findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(this);
     }
 
     @Override
     public void onSuccess(int requestId, Map<String, Object> result) {
-        String data=(String)result.get("data");
-        //Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
+        String urlList="http://192.168.0.35:8865/spring05/android/member/list.do";
+        //어떤 요청에 대한 결과인지 switch 문으로 분기한다.
+        switch (requestId){
+            case REQUEST_LIST:
+                String data=(String)result.get("data");
+                updateListView(data);
+                break;
+            case REQUEST_DELETE:
+                //스프링 웹서버에 요청하기
+                Util.sendGetRequest(REQUEST_LIST, urlList, null, this);
+                break;
+            case  REQUEST_INSERT:
+                //스프링 웹서버에 요청하기
+                Util.sendGetRequest(REQUEST_LIST, urlList, null, this);
+                break;
+        }
+    }
+    //ListView 를 업데이트 하는 메소드
+    public void updateListView(String data){
+        list.clear();
         try {
             JSONArray arr=new JSONArray(data);
-            for (int i=0; i<arr.length(); i++){
+            //반복문 돌면서
+            for(int i=0; i<arr.length(); i++){
+
                 JSONObject obj=arr.getJSONObject(i);
                 int num=obj.getInt("num");
                 String name=obj.getString("name");
-                //String addr=obj.getString("addr");
-
-                MemberDto dto=new MemberDto(num, name);
-                list.add(dto);
+                //회원정보를 MemberDto 객체를 생성해서 담은 다음
+                MemberDto tmp=new MemberDto(num, name, null);
+                //모델에 추가
+                list.add(tmp);
             }
+            //아답타에 모델의 데이터가 바뀌었다고 알려서 ListView 가 업데이트 되도록한다.
             adapter.notifyDataSetChanged();
-        }catch (JSONException e){
-            //Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+            //listview를 가장 아래 쪽으로 스크롤 하기(모델의 개수를 전달하면 된다.)
+            listView.smoothScrollToPosition(list.size());
+        } catch (JSONException e) {
+            e.printStackTrace();
         }
     }
 
     @Override
     public void onFail(int requestId, Map<String, Object> result) {
-        String msg=(String)result.get("data");
-        //Toast.makeText(this, "msg:"+msg, Toast.LENGTH_SHORT).show();
+        String data=(String)result.get("data");
+        Toast.makeText(this, data, Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    public void onItemClick(AdapterView<?> parent, View view, int i, long id) { // view 클릭한 셀view의 참조값 / i, id 클릭한 셀의 id값이 들어온다.
-        //1. 클릭한 셀의 회원 번호를 읽어와서
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        //1. 클릭한 셀의 회원번호를 읽어와서
         int num=list.get(i).getNum();
-
+        //2. Intent 객체에 담고
         Intent intent=new Intent(this, DetailActivity.class);
         intent.putExtra("num", num);
+        //3. DetailActivity 로 이동하기
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+        //클릭한 셀에 해당하는 회원정보 얻어오기
+        MemberDto dto=list.get(i);
+        //삭제할 회원의 번호를 필드에 저장한다.
+        selectedNum=dto.getNum();
+
+        new AlertDialog.Builder(this)
+                .setMessage(dto.getName()+" 의 정보를 삭제 하겠습니까?")
+                .setPositiveButton("확인", listener)
+                .setNegativeButton("취소", null)
+                .create()
+                .show();
+
+        return false;
+    }
+    //필드
+    private int selectedNum; // 삭제할 회원의 번호(PK)
+
+    DialogInterface.OnClickListener listener=new DialogInterface.OnClickListener() {
+        @Override
+        public void onClick(DialogInterface dialogInterface, int i) {
+            String urlAddr="http://192.168.0.35:8865/spring05" +
+                    "/android/member/delete.do";
+            //삭제할 회원의 번호를 Map 에 담고
+            Map<String, String> map=new HashMap<>();
+            map.put("num", Integer.toString(selectedNum));
+            //Util 을 이용해서 post 방식으로 요청을 한다.
+            Util.sendPostRequest(REQUEST_DELETE, urlAddr, map,
+                    Main4Activity.this);
+        }
+    };
+
+    @Override
+    public void onClick(View v) {
+        String name=inputName.getText().toString();
+        String addr=inputAddr.getText().toString();
+        Map<String, String> map=new HashMap<>();
+        map.put("name", name);
+        map.put("addr", addr);
+        String urlAddr="http://192.168.0.35:8865/spring05/android/member/insert.do";
+        Util.sendPostRequest(REQUEST_INSERT, urlAddr, map, Main4Activity.this );
+        //키보드 숨기기
+        Util.hideKeyboard(this);
+        //포커스 해제
+        Util.releaseFocus(inputName);
+        Util.releaseFocus(inputAddr);
+        //EditText 깨끗하게 만들어주기
+        inputName.setText("");
+        inputAddr.setText("");
     }
 }
