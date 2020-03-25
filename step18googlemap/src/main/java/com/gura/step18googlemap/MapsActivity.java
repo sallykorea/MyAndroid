d6aabbd50a8cb8921822c121fc751f78025d244e
kimhyeonkyung <khk5087@naver.com>
2020-03-25 10:47:47 +0900

    step18googlemap 설명 추가함

--- step18googlemap/src/main/java/com/gura/step18googlemap/MapsActivity.java
+++ step18googlemap/src/main/java/com/gura/step18googlemap/MapsActivity.java
@@ -22,13 +22,19 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //layout안에 button과 fragment가 있다.
+        //setContentView()메소드에의해 layout이 전개되면 findViewById()메소드를 객체의 참조값을 찾아서 쓸 수 있다.
         setContentView(R.layout.activity_main);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        //activity_main.xml에 있는 SupportMapFragment 객체의 참조값 얻어내기
+        SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map);
+        //지도가 동작할 준비가 완료됨을 감사힐 리스너 객체 등록하기
+        mapFragment.getMapAsync(this); //OnMapReadyCallback type을 인자로 보내준다.
         /*
-             fagment는 activity에의해 관리되는 객체.
-             fagment는 따로 UI를 가질 수 있다.
+             fragment는 activity에의해 관리되는 객체.
+             fragment는 따로 UI를 가질 수 있다.
+             fragment 자체는 view가 아니다. 따라서 findViewById() 메소드를 이용해서 참조값을 얻어낼 수 없다.
+             따라서 FragmentManger 객체를 이용해서 findFragmentById() 메소드를 이용해서 map 객체의 참조값을 찾아야한다.
+
          */
 
         Button moveBtn=findViewById(R.id.moveBtn);
@@ -60,7 +66,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         //지정한 위치와 배율로 카메라 이동하기
         CameraUpdate cu=CameraUpdateFactory.newLatLngZoom(citiyHall,15);
-        mMap.animateCamera(cu);
+        mMap.animateCamera(cu); //animateCamera() 메소드 : 카메라가 이동하는 중간과정을 보여준다.
     }
 
     @Override
